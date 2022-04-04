92399cd06659f24ca6edab472e2c22fd7a3b1156
Hemal Patel <hemal.patel7735@outlook.com>
2022-04-04 01:25:53 -0500

    perf-results: rw perf - two server

--- performance/rw_perf.cc
+++ performance/rw_perf.cc
@@ -20,40 +20,9 @@ using namespace std;
 
 uint64_t DIR_SIZE = 256*4096;
 uint64_t FILE_SIZE = 4096;
-int MAX_REQUESTS_PER_WORKER = 256;
+int MAX_REQUESTS_PER_WORKER = 64;
 
 BlockStorageClient *blockstorageClient;
-
-void warmup() {
-    string buffer(5, 'w');
-    int addr = 0;
-    
-    Status writeStatus = blockstorageClient->Write(addr, buffer);
-    cout<<writeStatus.error_code();
-    if (writeStatus.error_code() != grpc::StatusCode::OK) {
-        cout << "Write test failed" << endl;
-        return;
-    }
-
-    ReadRequest request;
-    ReadReply reply;
-    request.set_addr(addr);
-
-    Status readStatus = blockstorageClient->Read(request, &reply, addr);
-    
-    if (readStatus.error_code() != grpc::StatusCode::OK) {
-        cout << "Read test failed" << endl;
-        return;
-    }
-
-    if (reply.buffer().compare(buffer) == 0) {
-        cout << "TEST PASSED:" << endl;
-    } else {
-        cout << "TEST FAILED: Aligned read data is not the same as write buffer " << endl;
-        cout << "Read : " << reply.buffer() << endl;
-        cout << "Written: " << buffer << endl; 
-    }
-}
 
 void ping() {
     for(int i = 0; i < 2; i++) {
@@ -61,15 +30,10 @@ void ping() {
     }
 }
 
-uint64_t read_worker(int start_addr, int jump, int num_requests, int id) {
-    uint64_t time_taken = 0;
-
+int read_worker(int start_addr, int jump, int num_requests, int id) {
     ReadReply reply;
 
     for(int i = 0, addr = start_addr; i < num_requests; i++, addr += jump) {
-        // cout << "WORKER:" << id << " running addr:[" << addr << "]" << endl;
-        auto begin = chrono::high_resolution_clock::now();
-
         ReadRequest request;
         request.set_addr(addr);
 
@@ -79,19 +43,14 @@ uint64_t read_worker(int start_addr, int jump, int num_requests, int id) {
         if (readStatus.error_code() != grpc::StatusCode::OK) {
             cout << "Read test failed" << endl;
         }
-
-        auto end = chrono::high_resolution_clock::now();
-        time_taken += chrono::duration_cast<chrono::nanoseconds> (end - begin).count();
     }
-
-    return time_taken;
+    return 0;
 }
 
-uint64_t exec_reads(int N_workers, int num_requests, int jump_within_worker, int start_addr) {
+void exec_reads(int N_workers, int num_requests, int jump_within_worker, int start_addr) {
     // stores start addrs for each worker
     uint64_t start_addrs[N_workers];
-    future<uint64_t> workers[N_workers];
-    uint64_t time_taken = 0;
+    future<int> workers[N_workers];
 
     // prepare all addresses
     for(int addr = start_addr, i = 0; i < N_workers; i++, addr += DIR_SIZE) {
@@ -106,20 +65,20 @@ uint64_t exec_reads(int N_workers, int num_requests, int jump_within_worker, int
 
     // wait for them to finish
     for(int i = 0; i < N_workers; i++) {
-        time_taken += workers[i].get();
+        workers[i].get();
     }
-
-    return time_taken;
 }
 
-void read_performance() {
-    int workers = 1;
+void read_performance(int workers) {
 
     // 2 - read non-aligned wrt number of requests
     cout << "[Metrics_START:READ_NON_ALIGNED]" << endl;
     cout << "num_requests,time(in ms)" << endl;
     for(int num_request = 1; num_request <= MAX_REQUESTS_PER_WORKER; num_request*=2) {
-        uint64_t time_taken = exec_reads(workers, num_request, FILE_SIZE*3, FILE_SIZE + 2048);
+        auto begin = chrono::high_resolution_clock::now();
+        exec_reads(workers, num_request, FILE_SIZE*3, FILE_SIZE + 2048);
+        auto end = chrono::high_resolution_clock::now();
+        uint64_t time_taken = chrono::duration_cast<chrono::nanoseconds> (end - begin).count();
         cout << num_request*workers << "," << time_taken/1e6 << endl;
     }
     cout << "[Metrics_END:READ_NON_ALIGNED]" << endl;
@@ -128,40 +87,35 @@ void read_performance() {
     cout << "[Metrics_START:READ_ALIGNED]" << endl;
     cout << "num_requests,time(in ms)" << endl;
     for(int num_request = 1; num_request <= MAX_REQUESTS_PER_WORKER; num_request*=2) {
-        uint64_t time_taken = exec_reads(workers, num_request, FILE_SIZE*3, 0);
+        auto begin = chrono::high_resolution_clock::now();
+        exec_reads(workers, num_request, FILE_SIZE*3, 0);
+        auto end = chrono::high_resolution_clock::now();
+        uint64_t time_taken = chrono::duration_cast<chrono::nanoseconds> (end - begin).count();
         cout << num_request*workers << "," << time_taken/1e6 << endl;
     }
     cout << "[Metrics_END:READ_ALIGNED]" << endl;
 }
 
-uint64_t write_worker(int start_addr, int jump, int num_requests, int id) {
-    uint64_t time_taken = 0;
+int write_worker(int start_addr, int jump, int num_requests, int id) {
     string buffer(FILE_SIZE, 'w');
     Status writeStatus;
 
     for(int i = 0, addr = start_addr; i < num_requests; i++, addr += jump) {
-        // cout << "WORKER:" << id << " running addr:[" << addr << "]" << endl;
-        auto begin = chrono::high_resolution_clock::now();
-
         // actual call
         writeStatus = blockstorageClient->Write(addr, buffer);
 
         if (writeStatus.error_code() != grpc::StatusCode::OK) {
             cout << "Write test failed" << endl;
         }
-
-        auto end = chrono::high_resolution_clock::now();
-        time_taken += chrono::duration_cast<chrono::nanoseconds> (end - begin).count();
     }
 
-    return time_taken;
+    return 0;
 }
 
-uint64_t exec_writes(int N_workers, int num_requests, int jump_within_worker, int start_addr) {
+void exec_writes(int N_workers, int num_requests, int jump_within_worker, int start_addr) {
     // stores start addrs for each worker
     uint64_t start_addrs[N_workers];
-    future<uint64_t> workers[N_workers];
-    uint64_t time_taken = 0;
+    future<int> workers[N_workers];
 
     // prepare all addresses
     for(int addr = start_addr, i = 0; i < N_workers; i++, addr += DIR_SIZE) {
@@ -175,21 +129,20 @@ uint64_t exec_writes(int N_workers, int num_requests, int jump_within_worker, in
 
     // wait for them to finish
     for(int i = 0; i < N_workers; i++) {
-        time_taken += workers[i].get();
+        workers[i].get();
     }
-
-    return time_taken;
 }
 
-void write_performance() {
-    int workers = 1;
+void write_performance(int workers) {
 
      // 1 - read aligned wrt number of requests
     cout << "[Metrics_START:WRITE_ALIGNED]" << endl;
     cout << "num_requests,time(in ms)" << endl;
     for(int num_request = 1; num_request <= MAX_REQUESTS_PER_WORKER; num_request*=2) {
-        uint64_t time_taken = exec_writes(workers, num_request, FILE_SIZE*3, 0);
-        // uint64_t time_taken = write_worker(0, FILE_SIZE*3, num_request, );
+        auto begin = chrono::high_resolution_clock::now();
+        exec_writes(workers, num_request, FILE_SIZE*3, 0);
+        auto end = chrono::high_resolution_clock::now();
+        uint64_t time_taken = chrono::duration_cast<chrono::nanoseconds> (end - begin).count();
         cout << num_request*workers << "," << time_taken/1e6 << endl;
     }
     cout << "[Metrics_END:WRITE_ALIGNED]" << endl;
@@ -198,20 +151,22 @@ void write_performance() {
     cout << "[Metrics_START:WRITE_NON_ALIGNED]" << endl;
     cout << "num_requests,time(in ms)" << endl;
     for(int num_request = 1; num_request <= MAX_REQUESTS_PER_WORKER; num_request*=2) {
-        uint64_t time_taken = exec_writes(workers, num_request, FILE_SIZE*3, FILE_SIZE + 2048);
+        auto begin = chrono::high_resolution_clock::now();
+        exec_writes(workers, num_request, FILE_SIZE*3, FILE_SIZE + 2048);
+        auto end = chrono::high_resolution_clock::now();
+        uint64_t time_taken = chrono::duration_cast<chrono::nanoseconds> (end - begin).count();
         cout << num_request*workers << "," << time_taken/1e6 << endl;
     }
     cout << "[Metrics_END:WRITE_NON_ALIGNED]" << endl;
 
 }
 
-void write_congestion() {
-    int worker_limit = 8;
+void write_congestion(int worker_limit) {
 
     cout << "[Metrics_START:WRITE_ALIGNED_CONGESTION]" << endl;
     cout << "workers,time(in ms)" << endl;
     for(int num_worker = 1; num_worker <= worker_limit; num_worker++) {
-        future<uint64_t> workers[num_worker];
+        future<int> workers[num_worker];
         uint64_t time_taken = 0;
 
         for(int i = 0; i < num_worker; i++) {
@@ -230,7 +185,7 @@ void write_congestion() {
     cout << "workers,time(in ms)" << endl;
     int start_addr = FILE_SIZE/2;
     for(int num_worker = 1; num_worker <= worker_limit; num_worker++) {
-        future<uint64_t> workers[num_worker];
+        future<int> workers[num_worker];
         uint64_t time_taken = 0;
 
         for(int i = 0; i < num_worker; i++) {
@@ -246,13 +201,11 @@ void write_congestion() {
     cout << "[Metrics_END:WRITE_NON_ALIGNED_CONGESTION]" << endl;
 }
 
-void read_congestion() {
-    int worker_limit = 8;
-
+void read_congestion(int worker_limit) {
     cout << "[Metrics_START:READ_ALIGNED_CONGESTION]" << endl;
     cout << "workers,time(in ms)" << endl;
     for(int num_worker = 1; num_worker <= worker_limit; num_worker++) {
-        future<uint64_t> workers[num_worker];
+        future<int> workers[num_worker];
         uint64_t time_taken = 0;
 
         for(int i = 0; i < num_worker; i++) {
@@ -260,7 +213,7 @@ void read_congestion() {
 
         }
         for (int i = 0; i < num_worker; i++) {
-            time_taken += workers[i].get();
+            workers[i].get();
         }
 
         cout << num_worker << "," << time_taken/1e6 << endl;
@@ -271,7 +224,7 @@ void read_congestion() {
     cout << "workers,time(in ms)" << endl;
     int start_addr = FILE_SIZE/2;
     for(int num_worker = 1; num_worker <= worker_limit; num_worker++) {
-        future<uint64_t> workers[num_worker];
+        future<int> workers[num_worker];
         uint64_t time_taken = 0;
 
         for(int i = 0; i < num_worker; i++) {
@@ -287,25 +240,26 @@ void read_congestion() {
     cout << "[Metrics_END:READ_NON_ALIGNED_CONGESTION]" << endl;
 }
 
-// Usage: ./rw_perf 1 - just for warmup - no actual perf done
-// Usage: ./rw_perf 0 - just actual perf
+// Usage: ./rw_perf 1 4 - write with 4 workers
+// Usage: ./rw_perf 0 3 - read with 3 workers
 int main(int argc, char** argv) {
-    blockstorageClient = new BlockStorageClient(grpc::CreateChannel("0.0.0.0:50051", grpc::InsecureChannelCredentials()));
+    blockstorageClient = new BlockStorageClient(grpc::CreateChannel("128.105.144.16:50051", grpc::InsecureChannelCredentials()));
     string arg(argv[1]);
+    int workers = stoi(string(argv[2]));
 
     if (arg.compare("1") == 0) {
-        write_performance();
+        write_performance(workers);
     } else if (arg.compare("2") == 0) {
-        read_performance();
+        read_performance(workers);
     } else if (arg.compare("3") == 0) {
-        write_congestion();
+        write_congestion(workers);
     } else if (arg.compare("4") == 0) {
-        read_congestion();
+        read_congestion(workers);
     } else if (arg.compare("5") == 0) {
-        write_performance();
-        read_performance();
-        write_congestion();
-        read_congestion();
+        write_performance(workers);
+        read_performance(workers);
+        write_congestion(workers);
+        read_congestion(workers);
     } 
     
     return 0;

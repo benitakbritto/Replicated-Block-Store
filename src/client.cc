0ff3b362e8a132cd7b4c6f486fdfb49fbb8dede6
Hemal Patel <hemal.patel7735@outlook.com>
2022-04-04 19:03:39 -0500

    retry in client library

--- src/client.cc
+++ src/client.cc
@@ -1,13 +1,21 @@
 #include<errno.h>
 #include "client.h"
+#include <unistd.h>
 
 Status BlockStorageClient::Read(const ReadRequest request,
                   ReadReply* reply, int address) 
 {
-    // dbgprintf("[INFO] Read: Entering function\n");
-    ClientContext context;
     // dbgprintf("[INFO] Read: Invoking RPC\n");
-    Status status = stub_->Read(&context, request, reply);
+    int attempt = 0;
+    Status status;
+
+    do {
+        reply.clear();
+        ClientContext context;
+        status = stub_->Read(&context, request, reply);    
+        attempt++;
+    } while(attempt < 3 && status.error_code() != grpc::StatusCode::OK)
+    
     // dbgprintf("[INFO] Read: Exiting function\n");
     return status;
 }
@@ -18,9 +26,19 @@ Status BlockStorageClient::Write(int address,string buf)
     WriteRequest request;
     request.set_addr(address);
     request.set_buffer(buf);
-    WriteReply reply;
-    ClientContext context;
-    return stub_->Write(&context, request, &reply);
+    Status status;
+    int attempt = 0;
+    int backoff = 2;
+
+    do {
+        sleep(attempt * backoff);
+        WriteReply reply;
+        ClientContext context;
+        status = stub_->Write(&context, request, &reply);
+        attempt++;
+    } while(attempt < 3 && status.error_code() != grpc::StatusCode::OK)
+
+    return status;
 }
 
 Status BlockStorageClient::Ping() {
